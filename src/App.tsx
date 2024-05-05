2cd0a9640746b1501079cb739ad986ee8415a20a
namiqmamedov <namigmm@code.edu.az>
2024-05-06 02:03:34 +0400

    Initial create

---
+++ src/App.tsx
@@ -0,0 +1,50 @@
+import { Routes, Route } from 'react-router-dom'
+
+import './globals.css';
+import SignInForm from './_auth/forms/SignInForm';
+import SignUpForm from './_auth/forms/SignUpForm';
+import { Home } from './_root/pages';
+import AuthLayout from './_auth/AuthLayout';
+import RootLayout from './_root/RootLayout';
+import { Toaster } from '@/components/ui/toaster'
+import AllUsers from './_root/pages/AllUsers';
+import CreatePost from './_root/pages/CreatePost';
+import EditPost from './_root/pages/EditPost';
+import Explore from './_root/pages/Explore';
+import PostDetails from './_root/pages/PostDetails';
+import Profile from './_root/pages/Profile';
+import Saved from './_root/pages/Saved';
+import UpdateProfile from './_root/pages/UpdateProfile';
+import ErrorPage from './components/shared/ErrorPage';
+
+const App = () => {
+  return (
+        <main className='flex h-screen'>
+            <Routes>
+                {/* { public routes } */}
+                <Route element={<AuthLayout/>}>
+                    <Route path='/sign-in' element={<SignInForm />} />
+                    <Route path='/sign-up' element={<SignUpForm />} />
+                </Route>
+
+                {/* { private routes } */}
+                <Route element={<RootLayout/>}>
+                    <Route index element={<Home />} />
+                    <Route path='/explore' element={<Explore />} />
+                    <Route path='/saved' element={<Saved />} />
+                    <Route path='/all-users' element={<AllUsers />} />
+                    <Route path='/create-post' element={<CreatePost />} />
+                    <Route path='/update-post/:id' element={<EditPost />} />
+                    <Route path='/posts/:id' element={<PostDetails />} />
+                    <Route path='/profile/:id' element={<Profile />} />
+                    <Route path='/update-profile/:id' element={<UpdateProfile />} />
+                    <Route path='*' element={<ErrorPage />} />
+                </Route>
+            </Routes>
+
+            <Toaster />
+        </main>
+  )
+}
+
+export default App 
---
+++ src/components/shared/ErrorPage.tsx
@@ -0,0 +1,8 @@
+
+const ErrorPage = () => {
+  return (
+    <div className="pt-10 ml-5">Sorry, this page is doesn't exist.</div>
+  )
+}
+
+export default ErrorPage
